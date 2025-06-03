ddfd9435641f5f458ec6499cb9e4aa927bb49d0b
rahull0328 <rmmehta2004@gmail.com>
2025-06-03 20:13:49 +0530

    completed creating model and serialixer for books

--- Server/api/views.py
+++ Server/api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Book
+from .serializers import BookSerializer
 
 # Create your views here.
